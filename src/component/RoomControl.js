b5f17e5d96a4265f02fddb177b499f006ec63c44
bodwood <bodiewood@hotmail.com>
2023-02-10 13:51:02 -0800

    update name of edit frunction

--- src/component/RoomControl.js
+++ src/component/RoomControl.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import NewRoomForm from './NewRoomForm'
+import EditRoomForm from './EditRoomForm'
 import RoomList from './RoomList'
 import RoomDetail from './RoomDetail'
 import dataDefault from '../dataDefault'
@@ -9,43 +10,41 @@ export default class RoomControl extends React.Component {
     super(props)
     this.state = {
       formVisibleOnPage: false,
-      mainRoomList: dataDefault.data.room.sort((a, b) =>
-        a.name.localeCompare(b.name)
-      ),
+      mainRoomList: dataDefault.data.room.sort((a, b) => a.name.localeCompare(b.name)),
       selectedRoom: null,
       editing: false,
     }
   }
 
   handleClick = () => {
-   if(this.state.selectedRoom != null) {
-    this.setState({
-     formVisibleOnPage: false,
-     selecteRoom: null,
-     editing: false
-    });
-   } else {
-    this.setState(prevState => ({
-     formVisibleOnPage: !prevState.formVisibleOnPage
-    }));
-   }
+    if (this.state.selectedRoom != null) {
+      this.setState({
+        formVisibleOnPage: false,
+        selecteRoom: null,
+        editing: false,
+      })
+    } else {
+      this.setState((prevState) => ({
+        formVisibleOnPage: !prevState.formVisibleOnPage,
+      }))
+    }
   }
 
   handleDeletingRoom = (id) => {
-   const updatedRoomList = this.state.mainRoomList.filter(room => room.id != id);
-   this.setState({
-    mainRoomList: updatedRoomList.sort((a,b) => a.name.localeCompare(b.name)),
-    selectedRoom: null
-   })
+    const updatedRoomList = this.state.mainRoomList.filter(room => room.id != id)
+    this.setState({
+      mainRoomList: updatedRoomList.sort((a, b) => a.name.localeCompare(b.name)),
+      selectedRoom: null,
+    })
   }
 
-  hanldeEditClick = () => {
-   this.setState({editing: true})
+  handleEditClick = () => {
+    this.setState({ editing: true })
   }
 
   handleEditingRoomInList = (roomToEdit) => {
     const editedRoomList = this.state.mainRoomList
-      .filter((room) => room.id !== this.state.selectedRoom.id)
+      .filter(room => room.id !== this.state.selectedRoom.id)
       .concat(roomToEdit)
     this.setState({
       mainRoomList: editedRoomList.sort((a, b) => a.name.localeCompare(b.name)),
@@ -55,24 +54,26 @@ export default class RoomControl extends React.Component {
   }
 
   handleAddingNewRoomToList = (newRoom) => {
-   const updatedRoomList = this.state.mainRoomList.concat(newRoom);
-   this.setState({mainRoomList: updatedRoomList.sort((a,b) => a.name.localeCompare(b.name))});
-   this.setState({formVisibleOnPage: false});
+    const updatedRoomList = this.state.mainRoomList.concat(newRoom)
+    this.setState({mainRoomList: updatedRoomList.sort((a, b) => a.name.localeCompare(b.name))})
+    this.setState({ formVisibleOnPage: false })
   }
 
   handleChangingSelectedRoom = (id) => {
-   const selectedRoom = this.state.mainRoomList.filter(room => room.id === id)[0];
-   this.setState({selectedRoom: selectedRoom});
+    const selectedRoom = this.state.mainRoomList.filter(
+      room => room.id === id
+    )[0]
+    this.setState({ selectedRoom: selectedRoom })
   }
 
   handleSellingRoom = (id) => {
-   let roomToEdit = this.state.mainRoomList.filter(room => room.id === id)[0];
-   roomToEdit.stock > 0 ? roomToEdit.stock-- : alert("Room is not available");
-   const editedRoomList = this.state.mainRoomList
-    .filter(room => room.id !== id)
-    .concat(roomToEdit);
+    let roomToEdit = this.state.mainRoomList.filter(room => room.id === id)[0]
+    roomToEdit.stock > 0 ? roomToEdit.stock-- : alert('Room is not available')
+    const editedRoomList = this.state.mainRoomList
+      .filter(room => room.id !== id)
+      .concat(roomToEdit)
     this.setState({
-     mainRoomList: editedRoomList.sort((a,b) => a.name.localeCompare(b.name))
+      mainRoomList: editedRoomList.sort((a, b) => a.name.localeCompare(b.name)),
     })
   }
 
@@ -86,18 +87,35 @@ export default class RoomControl extends React.Component {
           onEditRoom={this.handleEditingRoomInList}
         />
       buttonText = 'Return to Room List'
-    } else if(this.state.selectedRoom != null) {
-      currentlyVisibleState = <RoomDetail room={this.state.selectedRoom} onClickingDelete={this.handleDeletingRoom} onClickingEdit={this.handleEditClick} onClickingSell={this.handleSellingRoom} />
-      buttonText= 'Return to Room List';
-    } else if(this.state.formVisibleOnPage) {
-     currentlyVisibleState = <NewRoomForm onNewRoomCreation={this.handleAddingNewRoomToList} />
-     buttonText= 'Return to Room List'
+    } else if (this.state.selectedRoom != null) {
+      currentlyVisibleState = 
+        <RoomDetail
+          room={this.state.selectedRoom}
+          onClickingDelete={this.handleDeletingRoom}
+          onClickingEdit={this.handleEditClick}
+          onClickingSell={this.handleSellingRoom}
+        />
+      buttonText = 'Return to Room List'
+    } else if (this.state.formVisibleOnPage) {
+      currentlyVisibleState = 
+        <NewRoomForm onNewRoomCreation={this.handleAddingNewRoomToList} />
+      buttonText = 'Return to Room List'
     } else {
-     currentlyVisibleState = <RoomList
-     onRoomSelection={this.handleChangingSelectedRoom}
-     onRoomSold={this.handleSellingRoom}
-     roomList={this.state.mainRoomList.sort((a,b) => a.name.localeCompare(b.name))} />
-     buttonText= 'Add Room'
+      currentlyVisibleState = 
+        <RoomList
+          onRoomSelection={this.handleChangingSelectedRoom}
+          onRoomSold={this.handleSellingRoom}
+          roomList={this.state.mainRoomList.sort((a, b) =>
+            a.name.localeCompare(b.name)
+          )}
+        />
+      buttonText = 'Add Room'
     }
+    return (
+      <main>
+        {currentlyVisibleState}
+        <button onClick={this.handleClick}>{buttonText}</button>
+      </main>
+    )
   }
 }
