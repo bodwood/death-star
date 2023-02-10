7eec58b25584ff1a70a3fa1ea4b236717c7e0f65
bodwood <bodiewood@hotmail.com>
2023-02-10 13:08:07 -0800

    add handleclick funtion to show form depending on edit, delete, add

--- src/component/RoomControl.js
+++ src/component/RoomControl.js
@@ -1,33 +1,62 @@
-import React from "react";
-import NewRoomForm from "./NewRoomForm"
-import RoomList from "./RoomList"
-import RoomDetail from "./RoomDetail"
-import dataDefault from "../dataDefault"
-
+import React from 'react'
+import NewRoomForm from './NewRoomForm'
+import RoomList from './RoomList'
+import RoomDetail from './RoomDetail'
+import dataDefault from '../dataDefault'
 
 export default class RoomControl extends React.Component {
-
- constructor(props) {
-  super(props);
-  this.state = {
-   formVisibleOnPage: false,
-   mainRoomList: dataDefault.data.room.sort((a, b) => a.name.localeCompare(b.name)),
-   selectedRoom: null,
-   editing: false
-  };
- }
-
- handleEditingRoomInList = (roomToEdit) => {
-  const fullRoomList = this.state.
- }
-
- render(){
-  let currentlyVisibleState = null;
-  let buttonText = null;
-  if(this.state.editing) {
-   currentlyVisibleState = <EditRoomForm room={this.state.selectedRoom} onEditRoom={this.handleEditingRoomInList} />
-   buttonText = "Return to Room List";
+  constructor(props) {
+    super(props)
+    this.state = {
+      formVisibleOnPage: false,
+      mainRoomList: dataDefault.data.room.sort((a, b) =>
+        a.name.localeCompare(b.name)
+      ),
+      selectedRoom: null,
+      editing: false,
+    }
   }
- }
 
+  handleClick = () => {
+   if(this.state.selectedRoom != null) {
+    this.setState({
+     formVisibleOnPage: false,
+     selecteRoom: null,
+     editing: false
+    });
+   } else {
+    this.setState(prevState => ({
+     formVisibleOnPage: !prevState.formVisibleOnPage
+    }));
+   }
+  }
+
+  hanldeEditClick = () => {
+   this.setState({editing: true})
+  }
+
+  handleEditingRoomInList = (roomToEdit) => {
+    const editedRoomList = this.state.mainRoomList
+      .filter((room) => room.id !== this.state.selectedRoom.id)
+      .concat(roomToEdit)
+    this.setState({
+      mainRoomList: editedRoomList.sort((a, b) => a.name.localeCompare(b.name)),
+      editing: false,
+      selecteRoom: null,
+    })
+  }
+
+  render() {
+    let currentlyVisibleState = null
+    let buttonText = null
+    if (this.state.editing) {
+      currentlyVisibleState = (
+        <EditRoomForm
+          room={this.state.selectedRoom}
+          onEditRoom={this.handleEditingRoomInList}
+        />
+      )
+      buttonText = 'Return to Room List'
+    }
+  }
 }
