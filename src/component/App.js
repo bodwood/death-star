0d5c921e1243eb46b54dc65f12b45b62baf66b77
bodwood <bodiewood@hotmail.com>
2023-02-10 10:46:06 -0800

    move App.js to component folder

---
+++ src/component/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg'
+import Header from './Header'
+import './App.css'
+
+function App() {
+  return (
+    <div className='wrapper'>
+      <Header />
+    </div>
+  )
+}
+
+export default App
