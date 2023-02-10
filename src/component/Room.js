16c9b853b3d37c54ba0d636f1075f5482506b350
bodwood <bodiewood@hotmail.com>
2023-02-10 11:39:41 -0800

    update room to include onclick function for when a room was sold

---
+++ src/component/Room.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Room = (props) => {
+  return (
+    <div className="room-card">
+     <div className="room-card-details" onClick={() => props.roomClicked(props.id)}>
+      <h2>{props.name} - {props.origin} - {props.roomColor} Room - ${props.price} /night</h2>
+     </div>
+     <div className='room-card-sell'>
+      <button className='room-card-button' onClick={() => props.roomSold(props.id)}>Rent Room {props.stock}</button>
+     </div>
+    </div>
+  )
+}
+
+Room.propTypes = {
+  id: PropTypes.string,
+  name: PropTypes.string,
+  origin: PropTypes.string,
+  onRoomClicked: PropTypes.func,
+  stock: PropTypes.number,
+}
+
+export default Room
