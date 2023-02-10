c24c6c52c8b7d476d374ea6d7394db1812a26e3c
bodwood <bodiewood@hotmail.com>
2023-02-10 12:21:30 -0800

    add values for creating new room (props)

--- src/component/NewRoomForm.js
+++ src/component/NewRoomForm.js
@@ -7,7 +7,23 @@ function NewRoomForm(props) {
  function handleNewRoomFormSubmission(e){
   e.preventDefault();
   props.onNewRoomCreation({
-   
+   name: e.target.name.value,
+   origin: e.target.origin.value,
+   price: e.target.price.value,
+   color: e.target.color.value,
+   description: e.target.description.value,
+   id: v4(),
+   stock: 12
   })
  }
+
+ return (
+  <>
+  <FormTemplate
+  formSubmissionHandler={handleNewRoomFormSubmission}
+  buttonText="Add Room" />
+  </>
+ )
 }
+
+export default NewRoomForm;
