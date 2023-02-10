5b0c0b30e9fcf2d6556df3849b4059fd179d0437
bodwood <bodiewood@hotmail.com>
2023-02-10 11:47:31 -0800

    update component to display details of individual rooms. Edit function is available

--- src/component/RoomDetail.js
+++ src/component/RoomDetail.js
@@ -3,5 +3,16 @@ import PropTypes from 'prop-types'
 
 function RoomDetail(props){
  const { room, onClickingDelete, onClickingEdit, onClickingSell } = props;
- 
+ return (
+  <div className="room-detail">
+   <h1>Room Details</h1>
+   <h2>{room.name} - {room.origin} - {room.color} - ${room.price} - Stock: {room.stock} left</h2>
+   <button onClick={() => onClickingSell(room.id)}>Rent Room - {room.stock} left</button>
+   <button onClick={onClickingEdit}>Edit Room</button>
+   <button onClick={() => onClickingDelete(room.id)}>Remove Room</button>
+   <p>{room.description}</p>
+   <p></p>
+
+  </div>
+ )
 }
